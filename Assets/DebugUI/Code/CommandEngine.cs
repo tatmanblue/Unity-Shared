b3389d3c123f9e3e4f944d983a7298b36ab453fd
matthew raffel <matt.raffel@gmail.com>
2022-01-17 12:31:40 -0700

    more coding
    Signed-off-by: matthew raffel <matt.raffel@gmail.com>

---
+++ Assets/DebugUI/Code/CommandEngine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using TatmanGames.DebugUI.Interfaces;
+
+namespace TatmanGames.DebugUI
+{
+    public delegate string GlobalCommandEvent(string command, string[] args);
+    public class CommandEngine
+    {
+        public List<IDebugCommand> Commands { get; } = new List<IDebugCommand>();
+        public event GlobalCommandEvent OnGlobalCommandEvent;
+
+        public string HandleCommand(string input)
+        {
+            // input is something like "player moveto 9999"
+            // the command is "player" and the arguments are "moveto" and "9999"
+            string[] elements = input.Split(' ');
+
+            IDebugCommand command = Commands.Find(c => 0 == c.Word.CompareTo(elements[0]));
+            string result = string.Empty;
+            
+            // if the command has not been found in the list do nothing
+            if (null == command)
+                return result;
+
+            if (true == command.HasCommandHandler())
+            {
+                result = command.FireCommandHandler(elements);
+            }
+            else
+            {
+                // there is an assumption here that there is one and only one global handler
+                // if someone has 2, the result is who knows what
+                GlobalCommandEvent global = OnGlobalCommandEvent;
+                if (null != global)
+                    result = global(elements[0], elements);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Assets/DebugUI/Code/Interfaces/IDebugCommand.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace TatmanGames.DebugUI.Interfaces
+{
+    public delegate string ExecuteCommandEvent(string[] args);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDebugCommand
+    {
+        string Word { get; }
+        string Description { get;  }
+
+        event ExecuteCommandEvent OnCommand;
+
+        bool HasCommandHandler();
+        string FireCommandHandler(string[] elemetns);
+    }
+
+    public abstract class DebugCommand : IDebugCommand
+    {
+        public string Word { get; protected set; }
+        public string Description { get; protected set; }
+        public event ExecuteCommandEvent OnCommand;
+        public bool HasCommandHandler()
+        {
+            ExecuteCommandEvent commandEvent = OnCommand;
+            if (commandEvent != null)
+                return true;
+
+            return false;
+        }
+
+        public string FireCommandHandler(string[] elements)
+        {
+            string result = string.Empty;
+            ExecuteCommandEvent commandEvent = OnCommand;
+            if (commandEvent != null)
+                result = commandEvent(elements);
+
+            return result;
+        }
+    }
+}
