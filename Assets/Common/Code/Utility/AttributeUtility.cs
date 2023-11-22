8dd4331cbb6deda26f5d23b757e72615ae11139a
Matt Raffel <matt.raffel@gmail.com>
2023-11-22 10:00:52 -0700

    adding helper method
    Signed-off-by: Matt Raffel <matt.raffel@gmail.com>

--- Assets/Common/Code/Utility/AttributeUtility.cs
+++ Assets/Common/Code/Utility/AttributeUtility.cs
@@ -56,4 +56,5 @@ namespace TatmanGames.Common.Utility
             return instances;
         }
     }
+    
 }
---
+++ Assets/Common/Code/Utility/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace TatmanGames.Common.Utility
+{
+    /// <summary>
+    /// see https://stackoverflow.com/questions/2656189/how-do-i-read-an-attribute-on-a-class-at-runtime
+    /// use
+    /// string name = typeof(MyClass).GetAttributeValue((CustomAttribute c) => c.WhateverProperty);
+    /// </summary>
+    public static class TypeExceptions
+    {
+        public static TValue GetAttributeValue<TAttribute, TValue>(
+            this Type type, 
+            Func<TAttribute, TValue> valueSelector) 
+            where TAttribute : Attribute
+        {
+            var att = type.GetCustomAttributes(
+                typeof(TAttribute), true
+            ).FirstOrDefault() as TAttribute;
+            if (att != null)
+            {
+                return valueSelector(att);
+            }
+            return default(TValue);
+        }
+    }
+}
